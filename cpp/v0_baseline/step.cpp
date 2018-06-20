bf8c89b4d51769e138f6a8454f90f318b0ce4916
Matias Lindgren <matias.lindgren@gmail.com>
2018-06-20 20:50:32 +0300

    Move step function to own file

---
+++ cpp/v0_baseline/step.cpp
@@ -0,0 +1,18 @@
+#include <limits>
+#include <algorithm>
+#include "step.hpp"
+
+void step(float* r, const float* d, int n) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            float v = std::numeric_limits<float>::infinity();
+            for (int k = 0; k < n; ++k) {
+                float x = d[n*i + k];
+                float y = d[n*k + j];
+                float z = x + y;
+                v = std::min(v, z);
+            }
+            r[n*i + j] = v;
+        }
+    }
+}
---
+++ cpp/v0_baseline/step.hpp
@@ -0,0 +1,6 @@
+#ifndef STEP_HPP
+#define STEP_HPP
+
+void step(float*, const float*, int);
+
+#endif // STEP_HPP
