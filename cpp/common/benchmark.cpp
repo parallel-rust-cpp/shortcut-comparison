8a7f0c24b445c2b3b41432088e9b7f595f076f0f
Matias Lindgren <matias.lindgren@gmail.com>
2018-06-20 23:39:47 +0300

    Add benchmarking

---
+++ cpp/common/benchmark.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <vector>
+#include "step.hpp"
+
+float next_float() {
+    static std::random_device rd;
+    static std::default_random_engine e(rd());
+    static std::uniform_real_distribution<float> floats(0.0, 1.0);
+    return floats(e);
+}
+
+void benchmark(const unsigned n) noexcept {
+    std::vector<float> result;
+    result.reserve(n*n);
+    std::vector<float> data;
+    data.reserve(n*n);
+    std::generate(data.begin(), data.end(), next_float);
+
+    auto time_start = std::chrono::high_resolution_clock::now();
+    step(result.data(), data.data(), n);
+    auto time_end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<float> delta_seconds = time_end - time_start;
+    std::cout << delta_seconds.count() << std::endl;
+}
+
+int main(int argc, const char** argv) {
+    if (argc < 2 || argc > 3) {
+        std::cerr << "usage: benchmark N [ITERATIONS]" << std::endl;
+        return EXIT_FAILURE;
+    }
+    const unsigned n = std::atoi(argv[1]);
+    const unsigned iterations = argc == 3 ? std::atoi(argv[2]) : 1;
+    std::cout << "benchmarking " << argv[0]
+              << " with input containing "
+              << n*n << " elements" << std::endl;
+
+    for (auto i = 0u; i < iterations; ++i) {
+        benchmark(n);
+    }
+}
