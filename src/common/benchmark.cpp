e5f2b51fb573ec64ae7150336888085050b6ebeb
Matias Lindgren <matias.lindgren@gmail.com>
2018-06-24 22:03:47 +0300

    Add tests for correctness

--- src/common/benchmark.cpp
+++ src/common/benchmark.cpp
@@ -3,8 +3,10 @@
 #include <iostream>
 #include <iomanip>
 #include <random>
+#include <string>
 #include <vector>
 #include "step.hpp"
+#include "step_reference.hpp"
 
 float next_float() {
     static std::random_device rd;
@@ -14,11 +16,9 @@ float next_float() {
 }
 
 void benchmark(const unsigned n) noexcept {
-    std::vector<float> result;
-    result.reserve(n*n);
-    std::vector<float> data;
-    data.reserve(n*n);
+    std::vector<float> data(n*n);
     std::generate(data.begin(), data.end(), next_float);
+    std::vector<float> result(n*n);
 
     const auto time_start = std::chrono::high_resolution_clock::now();
     step(result.data(), data.data(), n);
@@ -28,18 +28,84 @@ void benchmark(const unsigned n) noexcept {
     std::cout << std::setprecision(7) << delta_seconds.count() << std::endl;
 }
 
-int main(int argc, const char** argv) {
-    if (argc < 2 || argc > 3) {
-        std::cerr << "usage: " << argv[0] << " N [ITERATIONS]" << std::endl;
-        return EXIT_FAILURE;
+void test(const unsigned n) noexcept {
+    std::vector<float> data(n*n);
+    std::generate(data.begin(), data.end(), next_float);
+    std::vector<float> result_correct(n*n);
+    std::vector<float> result_testing(n*n);
+
+    step_reference(result_correct.data(), data.data(), n);
+    step(result_testing.data(), data.data(), n);
+
+    for (auto i = 0u; i < n*n; ++i) {
+        if (std::abs(result_testing[i] - result_correct[i]) > 1e-6) {
+            std::cerr << "\nERROR: step function produced unexpected value: "
+                      << result_testing[i]
+                      << ", at index " << i
+                      << ", while the reference solution produced "
+                      << result_correct[i] << std::endl;
+            break;
+        }
     }
-    const unsigned n = std::atoi(argv[1]);
-    const unsigned iterations = argc == 3 ? std::atoi(argv[2]) : 1;
-    std::cout << "benchmarking " << argv[0]
+}
+
+void run_benchmark(const unsigned n, const unsigned iterations) {
+    std::cout << "for " << iterations << " iterations"
               << " with input containing "
               << n*n << " elements" << std::endl;
-
     for (auto i = 0u; i < iterations; ++i) {
         benchmark(n);
+    }
+}
+
+void run_test(const unsigned n, const unsigned iterations) {
+    std::cout << "for " << iterations << " iterations"
+              << " with input containing "
+              << n*n << " elements" << std::endl;
+    for (auto i = 0u; i < iterations; ++i) {
+        test(n);
+        std::cout << '.' << std::flush;
+    }
+    std::cout << std::endl;
+}
+
+
+static const std::vector<std::string> VALID_COMMANDS {
+    "benchmark",
+    "test"
+};
+
+bool is_valid(const std::string& command) {
+    return (std::find(VALID_COMMANDS.begin(), VALID_COMMANDS.end(), command)
+            != VALID_COMMANDS.end());
+}
+
+void usage(const char* script_name) {
+    std::cerr << "usage: " << script_name << " <command> N [ITERATIONS]\n"
+              << "where command is one of:" << std::endl;
+    for (const auto& c : VALID_COMMANDS) {
+        std::cerr << "  " << c << std::endl;
+    }
+}
+
+int main(int argc, const char** argv) {
+    if (argc < 3 || argc > 4) {
+        usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+    const std::string command(argv[1]);
+    if (not is_valid(command)) {
+        usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+    const unsigned n = std::atoi(argv[2]);
+    const unsigned iterations = argc == 4 ? std::atoi(argv[3]) : 1;
+
+    if (command == "benchmark") {
+        std::cout << "benchmarking " << argv[0] << ' ' << std::flush;
+        run_benchmark(n, iterations);
+    } else if (command == "test") {
+        std::cout << "testing " << argv[0] << ' ' << std::flush;
+        run_test(n, iterations);
     }
 }
---
+++ src/common/step_reference.hpp
@@ -0,0 +1,7 @@
+#ifndef STEP_REFERENCE_HPP
+#define STEP_REFERENCE_HPP
+
+// Provides a baseline when testing for correctness
+void step_reference(float*, const float*, int);
+
+#endif // STEP_REFERENCE_HPP
