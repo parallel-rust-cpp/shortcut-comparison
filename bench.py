32402b60a667a1badb3d02e4e7082b8011a706ee
Matias Lindgren <matias.lindgren@gmail.com>
2018-06-27 23:09:00 +0300

    Start implementing rust v1 parallelization

--- bench.py
+++ bench.py
@@ -27,20 +27,15 @@ def parse_perf_csv(s):
     }
 
 
-def run_perf(cmd, num_threads=1):
+def run_perf(cmd):
     """
     Run given command string with perf-stat and return results in dict.
     """
     perf_cmd = "perf stat --detailed --detailed --field-separator ,".split(' ')
-    env = {
-        "OMP_NUM_THREADS": str(num_threads),
-        "OMP_PROC_BIND": "true"
-    }
     result = subprocess.run(
         perf_cmd + cmd.split(' '),
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
-        env=env
     )
     return parse_perf_csv(result.stdout.decode("utf-8"))
 
@@ -50,27 +45,32 @@ if __name__ == "__main__":
     parser.add_argument("build_dir",
         type=str,
         help="Path to the benchmark binaries")
-    parser.add_argument("input_element_bytes",
+    parser.add_argument("--limit_input_size", "-n",
         type=int,
-        help="Amount of bytes of a single element in the generated input")
+        default=len(INPUT_SIZES),
+        help="n in sizes[:n], where sizes is {}".format(INPUT_SIZES))
 
     args = parser.parse_args()
     build_dir = os.path.abspath(args.build_dir)
-    input_bytes = args.input_element_bytes
 
     print_header("Running perf-stat for all implementations", end="\n\n")
     for lang in ("cpp", "rust"):
         for step_impl in STEP_IMPLEMENTATIONS:
             print_header(lang + ' ' + step_impl)
             bench_cmd = os.path.join(build_dir, step_impl + "_" + lang)
-            print("N", 4*' ', "1e9 instr / sec")
+            print((5*"{:15s}").format("N", "time", "insn", "cycles", "insn/cyc"))
             for iterations in ITERATIONS:
-                for input_size in INPUT_SIZES[:7]:
+                for input_size in INPUT_SIZES[:args.limit_input_size]:
                     bench_args = 'benchmark {} {}'.format(input_size, iterations)
                     cmd = bench_cmd + ' ' + bench_args
                     result = run_perf(cmd)
-                    bns_ops_per_sec = 1e-9 * result["instructions"]/result["time"]
-                    print("{:4d}{:8.2f}".format(input_size, bns_ops_per_sec))
+                    insn_per_cycle = result["instructions"]/result["cycles"]
+                    print("{:4d}{:8.3f}{:15d}{:15d}{:8.3f}".format(
+                        input_size,
+                        result["time"],
+                        result["instructions"],
+                        result["cycles"],
+                        insn_per_cycle))
 
             print()
 
--- src/rust/v1_linear_reading/src/lib.rs
+++ src/rust/v1_linear_reading/src/lib.rs
@@ -1,26 +1,41 @@
 use std::vec;
 
+extern crate rayon;
+use rayon::prelude::*; // par_iter
+
 #[no_mangle]
-pub extern "C" fn step(r: *mut f32, d: *const f32, n: usize) {
+pub extern "C" fn step(r_raw: *mut f32, d_raw: *const f32, n: usize) {
+    // All data will be moved to intermediate containers because Rust does not
+    // allow references to const raw pointers in parallel sections
     let mut t: vec::Vec<f32> = vec![0.0; n * n];
-    for i in 0..n {
-        for j in 0..n {
-            t[n*j + i] = unsafe { *d.offset((n*i + j) as isize) };
-        }
-    }
+    let mut d: vec::Vec<f32> = vec![0.0; n * n];
+    let mut r: vec::Vec<f32> = vec![0.0; n * n];
 
     for i in 0..n {
         for j in 0..n {
+            let x = unsafe { *d_raw.offset((n*i + j) as isize) };
+            d[n*i + j] = x;
+            t[n*j + i] = x;
+        }
+    }
+
+    // Run each iteration of outer for loop in parallel
+    (0..n).into_par_iter().for_each(|i| {
+        for j in 0..n {
             let mut v = std::f32::INFINITY;
             for k in 0..n {
-                let x = unsafe { *d.offset((n*i + k) as isize) };
+                let x = d[n*i + k];
                 let y = t[n*j + k];
                 let z = x + y;
                 v = v.min(z);
             }
-            unsafe {
-                *r.offset((n*i + j) as isize) = v;
-            }
+            r[n*i + j] = v;
+        }
+    });
+
+    for (i, x) in r.iter().enumerate() {
+        unsafe {
+            *r_raw.offset(i as isize) = *x;
         }
     }
 }
