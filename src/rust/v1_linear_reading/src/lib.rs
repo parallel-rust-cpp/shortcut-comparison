099fb238d3b3a5433d8561ab3675cd96a8cde976
Matias Lindgren <matias.lindgren@gmail.com>
2018-06-28 21:18:48 +0300

    Fix parallel implementation of rust v1

--- src/rust/v1_linear_reading/src/lib.rs
+++ src/rust/v1_linear_reading/src/lib.rs
@@ -9,7 +9,6 @@ pub extern "C" fn step(r_raw: *mut f32, d_raw: *const f32, n: usize) {
     // allow references to const raw pointers in parallel sections
     let mut t: vec::Vec<f32> = vec![0.0; n * n];
     let mut d: vec::Vec<f32> = vec![0.0; n * n];
-    let mut r: vec::Vec<f32> = vec![0.0; n * n];
 
     for i in 0..n {
         for j in 0..n {
@@ -19,8 +18,11 @@ pub extern "C" fn step(r_raw: *mut f32, d_raw: *const f32, n: usize) {
         }
     }
 
-    // Run each iteration of outer for loop in parallel
-    (0..n).into_par_iter().for_each(|i| {
+    // Parallelize by creating an empty, mutable result vector, which is partitioned into rows and iterated in parallel.
+
+    let mut r: vec::Vec<f32> = vec![0.0; n * n];
+
+    r.par_chunks_mut(n).enumerate().for_each(|(i, row)| {
         for j in 0..n {
             let mut v = std::f32::INFINITY;
             for k in 0..n {
@@ -29,13 +31,11 @@ pub extern "C" fn step(r_raw: *mut f32, d_raw: *const f32, n: usize) {
                 let z = x + y;
                 v = v.min(z);
             }
-            r[n*i + j] = v;
+            row[j] = v;
         }
     });
 
     for (i, x) in r.iter().enumerate() {
-        unsafe {
-            *r_raw.offset(i as isize) = *x;
-        }
+        unsafe { *r_raw.offset(i as isize) = *x; }
     }
 }
