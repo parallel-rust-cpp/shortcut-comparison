8d18a395fb5a60cd381c93e144b4e4ee37d6b1a5
Matias Lindgren <matias.lindgren@gmail.com>
2018-06-29 23:01:31 +0300

    Fix

--- src/rust/tools/src/lib.rs
+++ src/rust/tools/src/lib.rs
@@ -9,14 +9,17 @@ impl Stopwatch {
     pub fn new() -> Stopwatch {
         Stopwatch { instants: vec::Vec::new() }
     }
+
     pub fn click(&mut self) {
         self.instants.push(time::Instant::now());
     }
-    pub fn dump(&self) {
+
+    pub fn report(&self) {
         self.instants.as_slice().windows(2).enumerate().for_each(|(i, w)| {
             let (earlier, later) = (w[0], w[1]);
             let elapsed = later.duration_since(earlier);
-            println!("{}-{} : {} ms", i, i+1, (1000 * elapsed.as_secs() + elapsed.subsec_millis() as u64));
+            let elapsed_ms = 1000 * elapsed.as_secs() + elapsed.subsec_millis() as u64;
+            println!("{}-{} : {} ms", i, i+1, elapsed_ms);
         });
     }
 }
