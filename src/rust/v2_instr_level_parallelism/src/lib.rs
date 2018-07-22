ae6d037dec2a31f868f0c255e3f5dedaac17053d
Matias Lindgren <matias.lindgren@gmail.com>
2018-07-22 15:54:01 +0300

    Fix Rust v2 bugs

--- src/rust/v2_instr_level_parallelism/src/lib.rs
+++ src/rust/v2_instr_level_parallelism/src/lib.rs
@@ -5,34 +5,33 @@ use rayon::prelude::*; // par_chunks_mut
 
 fn _step(r: &mut [f32], d: &[f32], n: usize) {
     const BLOCK_SIZE: usize = 4;
-    let block_count = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    let n_padded = block_count * BLOCK_SIZE;
+    let blocks_per_row = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    let n_padded = blocks_per_row * BLOCK_SIZE;
 
-    // Transpose of d
-    let mut t: vec::Vec<f32> = vec![0.0; n * n];
+    // Create d with right padding, and transpose of d
+    let mut vt: vec::Vec<f32> = vec![std::f32::INFINITY; n_padded * n];
+    let mut vd: vec::Vec<f32> = vec![std::f32::INFINITY; n_padded * n];
     for i in 0..n {
         for j in 0..n {
-            t[n*j + i] = d[n*i + j];
+            vd[n_padded*i + j] = d[n*i + j];
+            vt[n_padded*i + j] = d[n*j + i];
         }
     }
 
     // Partition the result slice into n rows, and compute result for each row in parallel
     r.par_chunks_mut(n).enumerate().for_each(|(i, row)| {
         for j in 0..n {
-            let mut block = vec![std::f32::INFINITY; BLOCK_SIZE];
-            for b in 0..block_count {
+            let mut block = [std::f32::INFINITY; BLOCK_SIZE];
+            for b in 0..blocks_per_row {
                 for k in 0..BLOCK_SIZE {
-                    let x = d[n_padded*i + b*BLOCK_SIZE + k];
-                    let y = t[n_padded*j + b*BLOCK_SIZE + k];
+                    let x = vd[n_padded * i + b * BLOCK_SIZE + k];
+                    let y = vt[n_padded * j + b * BLOCK_SIZE + k];
                     let z = x + y;
                     block[k] = block[k].min(z);
                 }
             }
-            let mut res = std::f32::INFINITY;
-            for x in block.iter() {
-                res = res.min(*x);
-            }
-            row[j] = res;
+            // Fold block values into a single minimum and assign to final result
+            row[j] = block.iter().fold(block[0], |acc, x| acc.min(*x));
         }
     });
 }
