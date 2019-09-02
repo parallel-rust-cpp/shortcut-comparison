a281754914ec35d8e7ce50981ebe619af8db7e67
Matias Lindgren <matias.lindgren@gmail.com>
2019-09-02 03:39:21 +0300

    encourage compiler to use registers in rust v4

--- src/rust/v4_register_reuse/src/lib.rs
+++ src/rust/v4_register_reuse/src/lib.rs
@@ -62,7 +62,15 @@ fn _step(r: &mut [f32], d: &[f32], n: usize) {
             //// ANCHOR_END: step_row_block_head
             //// ANCHOR: step_row_block_init
             // Partial results for 9 f32x8 row pairs
-            let mut tmp = [simd::f32x8_infty(); BLOCK_HEIGHT * BLOCK_HEIGHT];
+            let mut tmp0 = simd::f32x8_infty();
+            let mut tmp1 = simd::f32x8_infty();
+            let mut tmp2 = simd::f32x8_infty();
+            let mut tmp3 = simd::f32x8_infty();
+            let mut tmp4 = simd::f32x8_infty();
+            let mut tmp5 = simd::f32x8_infty();
+            let mut tmp6 = simd::f32x8_infty();
+            let mut tmp7 = simd::f32x8_infty();
+            let mut tmp8 = simd::f32x8_infty();
             // Extract all rows from the row blocks
             let mut vd_rows = vd_row_block.chunks_exact(vecs_per_row);
             let mut vt_rows = vt_row_block.chunks_exact(vecs_per_row);
@@ -75,16 +83,17 @@ fn _step(r: &mut [f32], d: &[f32], n: usize) {
             let block_combinations = izip!(vd_row_0, vd_row_1, vd_row_2, vt_row_0, vt_row_1, vt_row_2);
             for (&d0, &d1, &d2, &t0, &t1, &t2) in block_combinations {
                 // Combine all 9 pairs of f32x8 vectors from 6 rows at every column
-                tmp[0] = simd::min(tmp[0], simd::add(d0, t0));
-                tmp[1] = simd::min(tmp[1], simd::add(d0, t1));
-                tmp[2] = simd::min(tmp[2], simd::add(d0, t2));
-                tmp[3] = simd::min(tmp[3], simd::add(d1, t0));
-                tmp[4] = simd::min(tmp[4], simd::add(d1, t1));
-                tmp[5] = simd::min(tmp[5], simd::add(d1, t2));
-                tmp[6] = simd::min(tmp[6], simd::add(d2, t0));
-                tmp[7] = simd::min(tmp[7], simd::add(d2, t1));
-                tmp[8] = simd::min(tmp[8], simd::add(d2, t2));
+                tmp0 = simd::min(tmp0, simd::add(d0, t0));
+                tmp1 = simd::min(tmp1, simd::add(d0, t1));
+                tmp2 = simd::min(tmp2, simd::add(d0, t2));
+                tmp3 = simd::min(tmp3, simd::add(d1, t0));
+                tmp4 = simd::min(tmp4, simd::add(d1, t1));
+                tmp5 = simd::min(tmp5, simd::add(d1, t2));
+                tmp6 = simd::min(tmp6, simd::add(d2, t0));
+                tmp7 = simd::min(tmp7, simd::add(d2, t1));
+                tmp8 = simd::min(tmp8, simd::add(d2, t2));
             }
+            let tmp = [tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7, tmp8];
             //// ANCHOR_END: step_row_block_inner_loop
             //// ANCHOR: step_row_block_results
             // Set 9 final results for all combinations of 3 rows starting at i and 3 rows starting at j
@@ -93,7 +102,7 @@ fn _step(r: &mut [f32], d: &[f32], n: usize) {
                     let res_i = i * BLOCK_HEIGHT + block_i;
                     let res_j = j * BLOCK_HEIGHT + block_j;
                     if res_i < n && res_j < n {
-                        // Reduce one simd-vector to the final result for one pair of rows
+                        // Reduce one f32x8 to the final result for one pair of rows
                         r_row[res_j] = simd::horizontal_min(*tmp_res);
                     }
                 }
