1baf3b5d88d6c12f253a84745ed74c4c3bdc25ef
Matias Lindgren <matias.lindgren@gmail.com>
2019-09-09 20:40:17 +0300

    fix rust prefetch helper

--- src/rust/tools/src/simd.rs
+++ src/rust/tools/src/simd.rs
@@ -57,8 +57,8 @@ pub fn swap(v: f32x8, width: i8) -> f32x8 {
 }
 
 #[inline]
-pub fn prefetch(p: *const f32x8, length: isize) {
-    unsafe { _mm_prefetch((p as *const i8).offset(length), _MM_HINT_T0) }
+pub fn prefetch(p: *const f32x8, offset: isize) {
+    unsafe { _mm_prefetch(p.offset(offset) as *const i8, _MM_HINT_T0) }
 }
 
 /// Use an index to extract a single f32 from a 256-bit vector of single precision floats
