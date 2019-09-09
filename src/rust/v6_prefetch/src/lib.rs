700d4e2eae409f451fe07363bd8c078975ff8b3f
Matias Lindgren <matias.lindgren@gmail.com>
2019-09-09 20:40:54 +0300

    unroll rust v6 tmp array to avoid register spilling

--- src/rust/v6_prefetch/src/lib.rs
+++ src/rust/v6_prefetch/src/lib.rs
@@ -40,43 +40,50 @@ fn _step(r: &mut [f32], d: &[f32], n: usize) {
         .for_each(pack_simd_row);
 
     // Everything is exactly as in v5, but we add some prefetch instructions in the innermost loop
-    // Create raw pointers for prefetching
-    let step_row = |(i, (r_row_block, vd_row)): (usize, (&mut [f32], &[f32x8]))| {
-        let vd_ptr = vd.as_ptr();
-        let vt_ptr = vt.as_ptr();
-        assert_eq!(vd_row.len(), n);
+    let step_row_block = |(r_row_block, vd_row): (&mut [f32], &[f32x8])| {
+        // Create const raw pointers for specifying addresses to prefetch
+        let vd_row_ptr = vd_row.as_ptr();
+        const PREFETCH_LENGTH: usize = 20;
         for (j, vt_row) in vt.chunks_exact(n).enumerate() {
-            assert_eq!(vt_row.len(), n);
-            let mut tmp = [simd::f32x8_infty(); simd::f32x8_LENGTH];
+            let vt_row_ptr = vt_row.as_ptr();
+            let mut tmp0 = simd::f32x8_infty();
+            let mut tmp1 = simd::f32x8_infty();
+            let mut tmp2 = simd::f32x8_infty();
+            let mut tmp3 = simd::f32x8_infty();
+            let mut tmp4 = simd::f32x8_infty();
+            let mut tmp5 = simd::f32x8_infty();
+            let mut tmp6 = simd::f32x8_infty();
+            let mut tmp7 = simd::f32x8_infty();
             for (col, (&d0, &t0)) in vd_row.iter().zip(vt_row).enumerate() {
+                // Insert prefetch hints for fetching the cache line containing
+                // the memory address 20 addresses ahead of the current column
+                simd::prefetch(vd_row_ptr, (col + PREFETCH_LENGTH) as isize);
+                simd::prefetch(vt_row_ptr, (col + PREFETCH_LENGTH) as isize);
                 let d2 = simd::swap(d0, 2);
                 let d4 = simd::swap(d0, 4);
                 let d6 = simd::swap(d4, 2);
                 let t1 = simd::swap(t0, 1);
-                tmp[0] = simd::min(tmp[0], simd::add(d0, t0));
-                tmp[1] = simd::min(tmp[1], simd::add(d0, t1));
-                tmp[2] = simd::min(tmp[2], simd::add(d2, t0));
-                tmp[3] = simd::min(tmp[3], simd::add(d2, t1));
-                tmp[4] = simd::min(tmp[4], simd::add(d4, t0));
-                tmp[5] = simd::min(tmp[5], simd::add(d4, t1));
-                tmp[6] = simd::min(tmp[6], simd::add(d6, t0));
-                tmp[7] = simd::min(tmp[7], simd::add(d6, t1));
-                // Insert prefetch hints for 20 upcoming memory addresses in both vd_row and vt_row
-                simd::prefetch(vd_ptr, (i*n + col + 20) as isize);
-                simd::prefetch(vt_ptr, (i*n + col + 20) as isize);
+                tmp0 = simd::min(tmp0, simd::add(d0, t0));
+                tmp1 = simd::min(tmp1, simd::add(d0, t1));
+                tmp2 = simd::min(tmp2, simd::add(d2, t0));
+                tmp3 = simd::min(tmp3, simd::add(d2, t1));
+                tmp4 = simd::min(tmp4, simd::add(d4, t0));
+                tmp5 = simd::min(tmp5, simd::add(d4, t1));
+                tmp6 = simd::min(tmp6, simd::add(d6, t0));
+                tmp7 = simd::min(tmp7, simd::add(d6, t1));
             }
-            tmp[1] = simd::swap(tmp[1], 1);
-            tmp[3] = simd::swap(tmp[3], 1);
-            tmp[5] = simd::swap(tmp[5], 1);
-            tmp[7] = simd::swap(tmp[7], 1);
-            for block_i in 0..simd::f32x8_LENGTH {
-                for (block_j, r_row) in r_row_block.chunks_exact_mut(n).enumerate() {
-                    assert_eq!(r_row.len(), n);
-                    let res_i = block_j + i * simd::f32x8_LENGTH;
-                    let res_j = block_i + j * simd::f32x8_LENGTH;
-                    if res_i < n && res_j < n {
-                        let v = tmp[block_j ^ block_i];
-                        let vi = block_i as u8;
+            let tmp = [
+                tmp0, simd::swap(tmp1, 1),
+                tmp2, simd::swap(tmp3, 1),
+                tmp4, simd::swap(tmp5, 1),
+                tmp6, simd::swap(tmp7, 1),
+            ];
+            for (tmp_i, r_row) in r_row_block.chunks_exact_mut(n).enumerate() {
+                for tmp_j in 0..simd::f32x8_LENGTH {
+                    let res_j = j * simd::f32x8_LENGTH + tmp_j;
+                    if res_j < n {
+                        let v = tmp[tmp_i ^ tmp_j];
+                        let vi = tmp_j as u8;
                         r_row[res_j] = simd::extract(v, vi);
                     }
                 }
@@ -86,13 +93,11 @@ fn _step(r: &mut [f32], d: &[f32], n: usize) {
     #[cfg(not(feature = "no-multi-thread"))]
     r.par_chunks_mut(simd::f32x8_LENGTH * n)
         .zip(vd.par_chunks(n))
-        .enumerate()
-        .for_each(step_row);
+        .for_each(step_row_block);
     #[cfg(feature = "no-multi-thread")]
     r.chunks_mut(simd::f32x8_LENGTH * n)
         .zip(vd.chunks(n))
-        .enumerate()
-        .for_each(step_row);
+        .for_each(step_row_block);
 }
 
 
