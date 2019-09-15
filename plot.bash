2ee523b6321ca5e9a8ea1703effe74008c814d28
Matias Lindgren <matias.lindgren@gmail.com>
2019-09-15 15:13:01 +0300

    update plot script

--- plot.bash
+++ plot.bash
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-cpus=(Xeon_E3-1230_v5)
-output_dir=$HOME/code/shortcut-comparison-web/img
+cpus=(i5-4690k Xeon-E3-1230-v5)
+output_dir=plots
 
 for cpu in ${cpus[*]}; do
+	mkdir --parents --verbose ${output_dir}/${cpu}
 	python3 plot_reports.py \
-		reports/${cpu}/single_core \
-		--title Sequential \
-		--output-path ${output_dir}/${cpu}_single_core.png
+		reports/${cpu}/single-thread \
+		--title Single-core \
+		--output-path ${output_dir}/${cpu}/single_core.png
 	python3 plot_reports.py \
-		reports/${cpu}/multi_core \
-		--title 'Parallel, 4 threads' \
-		--output-path ${output_dir}/${cpu}_multi_core.png
+		reports/${cpu}/multi-thread \
+		--title Multi-core \
+		--output-path ${output_dir}/${cpu}/multi_core.png
 done
--- plot_reports.py
+++ plot_reports.py
@@ -2,94 +2,126 @@
 import argparse
 import collections
 import csv
-import os.path
+import os
 
-import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams.update({
     "svg.fonttype": "none",
     "font.family": "sans-serif",
-    "font.size": 30,
+    "font.size": 25,
 })
 
+COMPILERS = [
+    {"key": "gcc",
+     "display_name": "C++ (GCC)"},
+    {"key": "clang",
+     "display_name": "C++ (Clang)"},
+    {"key": "rustc",
+     "display_name": "Rust"},
+]
+colormap = plt.get_cmap("Accent", len(COMPILERS))
+for i, compiler in enumerate(COMPILERS):
+    compiler["color"] = colormap(i/len(COMPILERS))
 
-implementation_labels = tuple('v{}'.format(i) for i in range(8))
-langs = (
-    {"key": "cpp",
-     "display_name": "C++",
-     "github_color": "#f34b7d"},
-    {"key": "rust",
-     "display_name": "Rust",
-     "github_color": "#dea584"},
-)
+with open("src/step_implementations.txt") as f:
+    STEP_IMPLEMENTATIONS = [line.strip() for line in f]
 
 
-def parse_data_from_csv(report_path, lang, reduction="mean"):
-    data = collections.OrderedDict()
-    for label in implementation_labels:
-        with open(os.path.join(report_path, lang, label + ".csv")) as f:
-            data[label] = list(csv.DictReader(f))
-        if reduction == "mean":
-            results = data[label]
-            assert all(results[0]["N (rows)"] == res["N (rows)"] for res in results), "Unexpected differing input dataset sizes"
-            data[label] = collections.OrderedDict([
-                ("N (rows)", int(results[0]["N (rows)"])),
-                ("GFLOP/s", sum(float(result["GFLOP/s"]) for result in results)/len(results)),
-                ("time (us)", sum(int(result["time (us)"]) for result in results)/len(results)),
-                ("instructions", sum(int(result.get("instructions", 0)) for result in results)/len(results)),
-                ("cycles", sum(int(result.get("cycles", 0)) for result in results)/len(results)),
-            ])
-    return data
+def get_gflops(n, secs):
+    total_float_ops = 2 * (n ** 3)
+    return 1e-9 * total_float_ops / secs
+
+
+def parse_data_from_log(log_path):
+    with open(log_path) as f:
+        header = f.readline().strip()
+        assert header.startswith("benchmarking")
+        num_elems = int(header.split(" elements")[0].split(" ")[-1])
+        time = 0.0
+        num_iter = 0
+        for line in f:
+            line = line.strip()
+            if len(line.split(",")) > 1:
+                # No more iteration results
+                break
+            time += float(line)
+            num_iter += 1
+        assert num_iter > 0, "Unexpected amount of iterations: {}".format(num_iter)
+        # context-switches
+        f.readline()
+        # cpu-migrations
+        f.readline()
+        # page-faults
+        f.readline()
+        # cycles
+        cycles_line = f.readline().strip().split(",")
+        assert cycles_line[-1] == "GHz"
+        # instructions
+        instructions_line = f.readline().strip().split(",")
+        assert instructions_line[-1] == "insn per cycle"
+        return {"seconds": round(time/num_iter, 6),
+                "cycles": int(cycles_line[0]),
+                "instructions": int(instructions_line[0]),
+                "n": round(num_elems ** 0.5)}
 
 
 def plot_reports(title, report_path, metric):
-    fig = plt.figure(figsize=(12, 12))
+    fig = plt.figure(figsize=(14, 14))
     ax = fig.subplots()
-
-    index = np.arange(len(implementation_labels))
-    bar_width = 0.35
-    bar_sep = bar_width/8
-
+    index = list(range(len(STEP_IMPLEMENTATIONS)))
+    bar_width = 0.2
+    bar_sep = bar_width/6
     opacity = 1.0
-
-    lang_count = len(langs)
+    compilers = COMPILERS[:3]
+    num_compilers = len(compilers)
 
     if metric == "gflops":
-        get_metric = lambda res: res["GFLOP/s"]
-        ax.set_ylabel('GFLOP/s')
+        get_metric = lambda res: get_gflops(res["n"], res["seconds"])
+        ax.set_ylabel("GFLOP/s")
     elif metric == "seconds":
-        get_metric = lambda res: 1e-6 * res["time (us)"]
-        ax.set_ylabel('Running time (seconds)')
+        get_metric = lambda res: res["seconds"]
+        ax.set_ylabel("Running time (seconds)")
     elif metric == "instructions-per-cycle":
         get_metric = lambda res: res["instructions"] / res["cycles"]
-        ax.set_ylabel('Instructions per CPU cycle')
+        ax.set_ylabel("Instructions per CPU cycle")
     elif metric == "instructions":
         get_metric = lambda res: 1e-9 * res["instructions"]
-        ax.set_ylabel('Instructions (10^9)')
+        ax.set_ylabel("Billions (1e9) of instructions")
     elif metric == "cycles":
         get_metric = lambda res: 1e-9 * res["cycles"]
-        ax.set_ylabel('CPU cycles (10^9)')
+        ax.set_ylabel("Billions (1e9) of CPU cycles")
 
-    for i, lang in enumerate(langs):
-        data = parse_data_from_csv(report_path, lang["key"])
-        index_with_offset = index + i*bar_width + (i - lang_count//2)*bar_sep
-        gflops = tuple(get_metric(res) for res in data.values())
-        ax.bar(index_with_offset, gflops, bar_width,
-               alpha=opacity, color=lang["github_color"], label=lang["display_name"])
+    for i, compiler in enumerate(compilers):
+        data = [
+            parse_data_from_log(os.path.join(report_dir, compiler["key"], step + ".txt"))
+            for step in STEP_IMPLEMENTATIONS
+        ]
+        index_with_offset = [
+            idx + i*bar_width + i*bar_sep
+            for idx in index
+        ]
+        metrics = [get_metric(res) for res in data]
+        ax.bar(
+            index_with_offset,
+            metrics,
+            bar_width,
+            alpha=opacity,
+            label=compiler["display_name"],
+            color=compiler["color"]
+       )
 
-    ax.set_xlabel('Version')
-    ax.set_xticks(index + bar_width / 2)
-    ax.set_xticklabels(implementation_labels)
+    ax.set_xlabel("Implementation")
+    ax.set_xticks([idx + (num_compilers - 1) * (bar_width + bar_sep) / 2 for idx in index])
+    ax.set_xticklabels([step[:2] for step in STEP_IMPLEMENTATIONS])
     if title is not None:
         ax.set_title(title)
     ax.legend()
-
     fig.tight_layout()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Create bar plots from benchmark csv reports.")
-    parser.add_argument("csv_report_output_dir",
+    parser = argparse.ArgumentParser(description="Create bar plots from benchmark output.")
+    parser.add_argument("report_output_dir",
         type=str,
         help="Directory to load reports from.")
     parser.add_argument("--output-path",
@@ -103,21 +135,20 @@ if __name__ == "__main__":
     parser.add_argument("--metric",
         type=str,
         choices=(
-            "gflops",
-            "seconds",
-            "instructions-per-cycle",
-            "instructions",
             "cycles",
+            "gflops",
+            "instructions",
+            "instructions-per-cycle",
+            "seconds",
         ),
         default="gflops",
-        help="Metric to be used on the vertical axis. Defaults to GFLOP per second.")
+        help="Metric to be used on the vertical axis.")
     args = parser.parse_args()
-    report_dir = args.csv_report_output_dir
+    report_dir = args.report_output_dir
     plot_reports(args.title, report_dir, args.metric)
     if not args.output_path:
         output_img_path = os.path.join(report_dir, "plot." + args.type)
     else:
         output_img_path = args.output_path
-
     plt.savefig(output_img_path)
     print("Wrote plot to {}".format(output_img_path))
